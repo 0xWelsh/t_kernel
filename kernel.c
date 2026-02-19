5b17e2c24c291828cb8ad11eeb897b3b379ec177
0xWelsh <maguamuhuri@gmail.com>
2026-02-19 16:35:52 +0300

    print message, initialize IDT, trigger interrupt, infinite loop

--- kernel.c
+++ kernel.c
@@ -1,20 +1,24 @@
 #include <stddef.h>
 #include <stdint.h>
+#include "idt.h"
 
 void kernel_main(void) {
 
-	volatile char* video = (volatile char*)0xB8000;
+	volative char* video = (volative char*)0xB8000;
 
 	const char* message = "Kernel loaded successfully.";
 	for (size_t i = 0; message[i] != '\0'; i++) {
 	
 		video[i * 2] = message[i];
-		video[i * 2 + 1] = 0x07; // light grey on black
+		video[i * 2 + 1] = 0x07;
 	}
+
+	// initialize interrupt descriptor table
+	idt_init();
+
+	// trigger interrupt 0 manually
+	__asm__ __volatile__("int $0");
 
 	while (1) {
-	
-		// infinite loop so we don't return
 	}
-
 }
